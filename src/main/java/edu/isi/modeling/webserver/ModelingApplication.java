c85392e06476163f08d191fb6fd4491ff6b54808
mohsen taheriyan <mohsen@isi.edu>
2016-03-31 09:35:33 -0700

    updated api version

--- src/main/java/edu/isi/modeling/webserver/ModelingApplication.java
+++ src/main/java/edu/isi/modeling/webserver/ModelingApplication.java
@@ -44,7 +44,7 @@ public class ModelingApplication extends Application {
         super();
 
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("0.1");
+        beanConfig.setVersion("1.0.0");
         beanConfig.setSchemes(new String[]{"http"});
         beanConfig.setHost("localhost:8080");
 //        beanConfig.setBasePath("/ModelingRESTJersey");
